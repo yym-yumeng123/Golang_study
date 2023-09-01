580a4417713e2f5d31dcadf52502a3080a3af4a3
yym-yumeng123 <18026493873@163.com>
2023-09-01 20:39:10 +0800

    基本数据类型

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Golang_study
+
 从零学习Golang
+
+### 运行 go 代码
+
+```bash
+go run xxx.go
+```
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main()  {
+	println("Hello World")
+
+
+	// 变量
+	// var name string = "yym"
+	name := "yym_yu"
+	println(name)
+
+	const name1 = "张三"
+	// 无法修改
+	// name1 = "李四"
+}

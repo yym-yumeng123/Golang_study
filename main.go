2346bcbcd0fc697e2c4eca5f7e468b766f1ca469
yym-yumeng123 <18026493873@163.com>
2023-09-01 21:01:05 +0800

    类型转换

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+// import (
+// 	"fmt"
+// 	"os"
+// )
+
 func main()  {
 	println("Hello World")
 
@@ -12,4 +17,9 @@ func main()  {
 	const name1 = "张三"
 	// 无法修改
 	// name1 = "李四"
+
+	var i = 100
+	var f float32
+	f = float32(i)
+	println(f)
 }

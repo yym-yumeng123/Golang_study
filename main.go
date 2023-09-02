4107951a970ce649ec00cc8919a30d872b3528cc
yym-yumeng123 <18026493873@163.com>
2023-09-02 11:16:19 +0800

    指针了解

--- main.go
+++ main.go
@@ -18,8 +18,20 @@ func main()  {
 	// 无法修改
 	// name1 = "李四"
 
+	// var i = 100
+	// var f float32
+	// f = float32(i)
+	// println(f)
+
 	var i = 100
-	var f float32
-	f = float32(i)
-	println(f)
+	var ip *int
+	// 通过 & 拿到内存地址
+	ip = &i
+
+	println(i)// 100
+	// 读取 i 的值
+	println(*ip) // 100
+	// 修改 i 的值
+	*ip = 200
+	println(i) // 200
 }

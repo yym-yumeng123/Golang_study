e61cbf82facf66cfb9d89864d3d25cf0cd928cd5
yym-yumeng123 <18026493873@163.com>
2023-10-28 19:18:33 +0800

    原码反码补码位运算

--- example/practice/进制.go
+++ example/practice/进制.go
@@ -15,5 +15,19 @@ func main() {
 	var k int = 0x11
 	fmt.Println("k=", k)
 
-	// 其他进制转十进制
+	// 位运算
+	var a int = 1 >> 2
+	var b int = -1 >> 2
+	var c int = 1 << 2
+	var d int = -1 << 2
+	fmt.Println("a=", a)
+	fmt.Println("b=", b)
+	fmt.Println("c=", c)
+	fmt.Println("d=", d)
+
+	fmt.Println("2&3", 2&3)
+	fmt.Println("13&7", 13&7)
+	fmt.Println("2|3", 2|3)
+	fmt.Println("5|4", 5|4)
+	fmt.Println("-3^3", -3^3)
 }

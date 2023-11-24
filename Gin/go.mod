f477106abf72926472aa071bcfc4a96b99f4e2c0
yym-yumeng123 <18026493873@163.com>
2023-11-24 18:37:52 +0800

    gorm  ini

--- Gin/go.mod
+++ Gin/go.mod
@@ -42,6 +42,7 @@ require (
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.2 // indirect
 	gorm.io/gorm v1.25.5 // indirect
--- Gin/routesGroup/main.go
+++ Gin/routesGroup/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"gin/routesGroup/routes"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/ini.v1"
+	"os"
 )
 
 func main() {
@@ -13,5 +16,17 @@ func main() {
 	routes.DefaultRoutesInit(r)
 	routes.ArticleRoutesInit(r)
 
+	cfg, err := ini.Load("../conf/app.ini")
+
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+
+	// 典型读取操作，默认分区可以使用空字符串表示
+	fmt.Println("App Mode:", cfg.Section("").Key("app_mode").String())
+	fmt.Println("Data Path:", cfg.Section("paths").Key("data").String())
+
 	r.Run()
+
 }
